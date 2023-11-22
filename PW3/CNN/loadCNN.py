1cc911b6e597f6d132ae4430c7ed21f29585a096
maudtst <mauddtissot@gmail.com>
2023-11-22 19:31:41 +0100

    Ajouter le fichier volumineux CNN.zip

---
+++ PW3/CNN/loadCNN.py
@@ -0,0 +1,19 @@
+import pickle
+
+def loadCNN():
+	file = open("CNN/CNNArticles",'rb')
+	articles = pickle.load(file)
+	file = open("CNN/CNNGold",'rb')
+	abstracts = pickle.load(file)
+
+	articlesCl = []  
+	for article in articles:
+		articlesCl.append(article.replace("”", "").rstrip("\n"))
+	articles = articlesCl
+	  
+	articlesCl = []  
+	for article in abstracts:
+		articlesCl.append(article.replace("”", "").rstrip("\n"))
+	abstracts = articlesCl
+    
+	return articles, abstracts
